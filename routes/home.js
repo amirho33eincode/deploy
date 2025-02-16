9584d53a55a87a4b61c10bd44d48b8c9849526da
Amir <Amir@gmail.com>
2025-02-16 14:41:04 -0800

    ali

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
+    title: "Express Testing deployment",
     message: "The app is working properly!",
   });
 });
